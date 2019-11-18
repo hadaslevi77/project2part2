59b2404e0e34864b9f1dd85a1bc71ab5cacd508b
hadalevi <hadalevi@T-SINAI11>
2019-11-18 17:44:47 +0200

    new

--- project2/Program.cs
+++ project2/Program.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace project2
 {
+    class GuestRequest
+    {
+        public string EntryDate { get; set; }
+        public string ReleaseDate { get; set; }
+        public bool IsApproved { get; set; }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hadas");
+            string a, b;
+            bool c;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = true;
+            GuestRequest guest = new GuestRequest();
+            guest.EntryDate = a;
+            guest.ReleaseDate = b;
+            guest.IsApproved = c;
+            Console.WriteLine(guest.ToString());
         }
     }
 }
